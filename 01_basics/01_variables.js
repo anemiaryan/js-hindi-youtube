edafd9be53d82823755857a9e659a0df05bc7043
anemiaryan <156048821+anemiaryan@users.noreply.github.com>
2024-11-10 05:47:32 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accoutId = 12345
+let accountEmail = "domt@domy.com"
+var accountPassword = "787878" 
+accuntCity = "Mumbai"
+
+accountEmail = "aryan@gman.com"
+accountPassword="878787"
+accuntCity = "Bhubaneswar"
+
+console.table([accoutId,accountEmail,accountPassword,accuntCity])
+
+
+
+// prfer not to use var 
+// because of issue in block scope and functional scope
