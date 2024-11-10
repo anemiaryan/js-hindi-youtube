8c2a35b31087b7439a044e1e6de8b9d1827aca12
anemiaryan <156048821+anemiaryan@users.noreply.github.com>
2024-11-10 05:49:12 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"  //treat all js code as newer version
+
+// alert(3 + 3) //we are using node js not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+console.log("Aryan")
+
+
+let name = "Aryan"
+let age = 21
+let isLoggedIn = false
+let state=null //it should not represent with empty string ("")
+
+//primitive:
+// number => 2 to power 53
+// bigint => need in stock market,trading,facebbok(big application)
+//string => ""
+//boolean => true/false
+//null => standalone value
+//symbol => uniqueness of component (react,figma)
+
+
+
+// non-primitive:
+// object
+
+console.log(typeof null) // print object
+console.log(typeof undefined) // print undefined
