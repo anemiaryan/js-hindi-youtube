d8e16279cc1a8d5e16bc1dfe6cd4b2c0e7235a3c
anemiaryan <156048821+anemiaryan@users.noreply.github.com>
2024-11-11 10:22:37 +0000

    ok

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,4 @@
-console.log("Aryan")
+const name = "Aryan"
+const repoCount = 50 
+console.log(name + repoCount + "value")
+console.log(`Hello my name is ${name} and i have completed ${repoCount} videos`)
